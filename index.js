0b61899584756a38006e95d0b80b1f29a3e67a49
VanessA594 <kallasvanessa72@gmail.com>
2024-12-09 17:24:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,4 +2,6 @@
   <a href="index.html">Avaleht</a> | 
   <a href="tasks.html">Ülesanded</a> | 
   <a href="contact.html">Kontakt</a>
+  <button class="start-btn" onclick="window.location.href='uulehekylg.html';">Alusta harjutamist</button>
+
 </nav>
